ed1ebc6578b0f2799bc2c2bd7caa94bca7ce6453
Mitzi Zitler <mitzitler@gmail.com>
2026-05-26 12:20:48 -0400

    retyped all blog files as svx

--- src/imports.ts
+++ src/imports.ts
@@ -3,31 +3,31 @@ interface DDRoute {
 }
 
 export const locationsRoutes: DDRoute = {
-    'Phandalin': ['src/routes/locations/phandalin/+page.md', 'Phandalin'], 
-    'Dwarven Excavation': ['src/routes/locations/dwarven_excavation/+page.md', 'DwarvenExcavation'], 
-    'Gnomenguard': ['src/routes/locations/gnomenguard/+page.md', 'Gnomenguard'], 
-    'Butterskull Ranch': ['src/routes/locations/butterskull_ranch/+page.md', 'ButterskullRanch'], 
-    "Logger's Camp": ['src/routes/locations/loggers_camp/+page.md', 'LoggersCamp'], 
-    "Mountain's Toe Gold Mine": ['src/routes/locations/mountains_toe_gold_mine/+page.md', 'MountainsToe'],
+    'Phandalin': ['src/routes/locations/phandalin/+page.svx', 'Phandalin'], 
+    'Dwarven Excavation': ['src/routes/locations/dwarven_excavation/+page.svx', 'DwarvenExcavation'], 
+    'Gnomenguard': ['src/routes/locations/gnomenguard/+page.svx', 'Gnomenguard'], 
+    'Butterskull Ranch': ['src/routes/locations/butterskull_ranch/+page.svx', 'ButterskullRanch'], 
+    "Logger's Camp": ['src/routes/locations/loggers_camp/+page.svx', 'LoggersCamp'], 
+    "Mountain's Toe Gold Mine": ['src/routes/locations/mountains_toe_gold_mine/+page.svx', 'MountainsToe'],
 };
 
 export const plotsRoutes: DDRoute = {
-    'Zhentarim': ['text/dm_content/plotlines/zhentarim.md', 'Zhentarim'],
-    'Harpers': ['text/dm_content/plotlines/harpers.md', 'Harpers'],
-    'The Iron Throne': ['text/dm_content/plotlines/ironthrone.md', 'IronThrone'], 
-    'Anchorites of Talos': ['text/dm_content/plotlines/anchorites.md', 'Anchorites']
+    'Zhentarim': ['text/dm_content/plotlines/zhentarim.svx', 'Zhentarim'],
+    'Harpers': ['text/dm_content/plotlines/harpers.svx', 'Harpers'],
+    'The Iron Throne': ['text/dm_content/plotlines/ironthrone.svx', 'IronThrone'], 
+    'Anchorites of Talos': ['text/dm_content/plotlines/anchorites.svx', 'Anchorites']
 };
 
 export const extrasRoutes: DDRoute = {
-    'Secret Goals': ['text/dm_content/secret_goals/0full_goals_descr.md', 'SecretGoals'],
-    'Side Quests':  ['text/dm_content/side_quests/0full_side_quests_descr.md', 'SideQuests']
+    'Secret Goals': ['text/dm_content/secret_goals/0full_goals_descr.svx', 'SecretGoals'],
+    'Side Quests':  ['text/dm_content/side_quests/0full_side_quests_descr.svx', 'SideQuests']
 }
 
 export const refRoutes: DDRoute = {
-    'NPCs': ['text/npc_content/all_npcs.md', 'NPCs'],
-    'Items': ['text/npc_content/magic_items.md', 'Items'],
-    'Players': ['text/dm_content/players/all_players.md', 'Players'],
-    'Prints': ['text/npc_content/prints_list.md', 'Prints'],
+    'NPCs': ['text/npc_content/all_npcs.svx', 'NPCs'],
+    'Items': ['text/npc_content/magic_items.svx', 'Items'],
+    'Players': ['text/dm_content/players/all_players.svx', 'Players'],
+    'Prints': ['text/npc_content/prints_list.svx', 'Prints'],
 }
 
 export interface Monster {
@@ -76,8 +76,4 @@ export const enum Alignments {
     LawfulEvil, 
     NeutralEvil,
     ChaoticEvil
-}
-
-export interface MonsterAttributes {
-    [actionName: string] : string
 }
--- src/state.store.js
+++ src/state.store.js
@@ -1,6 +1,6 @@
 export const siteState = $state({
-	mainPaneContent: ['text/dm_content/locations/phandalin.md', 'Phandalin'],
-    sidePaneContent: ['text/npc_content/all_npcs.md', 'NPCs'],
+	mainPaneContent: ['text/dm_content/locations/phandalin.svx', 'Phandalin'],
+    sidePaneContent: ['text/npc_content/all_npcs.svx', 'NPCs'],
     activeOpen: "",
 	/* ... */
 });
