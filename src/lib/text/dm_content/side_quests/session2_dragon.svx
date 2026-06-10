0a54857ca3287965eb76cdd8ccd42bdbcb48264f
Mitzi Zitler <mitzitler@gmail.com>
2026-06-10 11:09:47 -0400

    all the text documents moved into a new path under the src folder

---
+++ src/lib/text/dm_content/side_quests/session2_dragon.svx
@@ -0,0 +1,9 @@
+### Dragon Encounter
+
+For: All
+
+Given by: Cryovain
+
+Session: 2
+
+On their way out of town for either Gnomenguard or the Dwarven Excavation, they encounter Cryovain for the first time. They can choose to fight, and he will leave as soon as he takes 10 damage.
--- src/lib/text/ref_layout.svelte
+++ src/lib/text/ref_layout.svelte
@@ -3,64 +3,64 @@
     // let { children } = props;
 </script>
 
-<main class="doc-content">
+<main class="ref-content">
     {@render children()}
 </main>
 
 <style>
 
-    .doc-content :global(main) {
+    .ref-content :global(main) {
         font-family: 'Krungthep';
         color: var(--color-idx-2);
         font-weight: 200;
-        font-size: 14px;
+        font-size: 10px;
     }
-    .doc-content :global(h1) {
+    .ref-content :global(h1) {
         margin-left: 20px;
         color: var(--color-idx-2);
         margin-top: 6px;
-        font-size: 26px;
+        font-size: 22px;
         font-weight: 500;
     }
 
-    .doc-content :global(h2) {
-        margin-left: 20px;
-        color: var(--color-idx-2);
-        margin-top: 4px;
-        font-size: 22px;
-    }
-
-    .doc-content :global(h3) {
+    .ref-content :global(h2) {
         margin-left: 20px;
         color: var(--color-idx-2);
         margin-top: 4px;
         font-size: 18px;
-        font-weight: 400;
-        text-decoration: underline;
     }
 
-    .doc-content :global(h4) {
+    .ref-content :global(h3) {
         margin-left: 20px;
         color: var(--color-idx-2);
         margin-top: 4px;
         font-size: 14px;
         font-weight: 400;
+        text-decoration: underline;
     }
 
-    .doc-content :global(p) {
+    .ref-content :global(h4) {
+        margin-left: 20px;
+        color: var(--color-idx-2);
+        margin-top: 4px;
+        font-size: 10px;
+        font-weight: 400;
+    }
+
+    .ref-content :global(p) {
         margin-left: 10px;
         margin-bottom: 10px;
         color: var(--color-idx-2);
-        font-size: 14px;
+        font-size: 10px;
     }
 
-    .doc-content :global(li) {
+    .ref-content :global(li) {
         margin-left: 20px;
         color: var(--color-idx-2);
-        font-size: 14px;
+        font-size: 10px;
     }
 
-    /* .doc-content :global(li::before) {
+    /* .ref-content :global(li::before) {
         content: "•"; 
         position: absolute;
 
@@ -68,16 +68,16 @@
         color: var(--color-idx-3); 
     } */
 
-    .doc-content :global(a) {
+    .ref-content :global(a) {
         color: var(--color-idx-7);
         text-decoration: underline;
     }
 
-    .doc-content :global(a:hover) {
+    .ref-content :global(a:hover) {
         color: var(--color-idx-4)
     }
 
-    .doc-content :global(blockquote), :global(img) {
+    .ref-content :global(blockquote), :global(img) {
         /* margin-left: 30px;
         margin-right: 30px;
         margin-top: 10px; */
@@ -87,13 +87,12 @@
         font-style: italic;
     }
 
-    .doc-content :global(strong) {
+    .ref-content :global(strong) {
         font-weight: 800;
     }
 
-    .doc-content :global(img) {
+    .ref-content :global(img) {
         margin: 15px 40px;
-        max-width: 50%;
         background-color: var(--color-idx-9);
     }
 
